7066e2736ba4224714910e69857b393533b99add
RToramaru <rafael.almeida.soares2012@gmail.com>
2023-03-23 23:09:54 -0300

    criacao do store com mobx

---
+++ lib/view/store/finance_store.dart
@@ -0,0 +1,141 @@
+import 'package:finance/controller/finance_controller.dart';
+import 'package:finance/model/finance_model.dart';
+import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
+import 'package:mobx/mobx.dart';
+import 'package:uuid/uuid.dart';
+
+part 'finance_store.g.dart';
+
+class FinanceStore = FinanceStoreBase with _$FinanceStore;
+
+abstract class FinanceStoreBase with Store {
+  FinanceController financeController = FinanceController();
+
+  @observable
+  int currentIndex = 1;
+
+  @observable
+  PageController pageController = PageController();
+
+  @observable
+  TextEditingController descriptionController = TextEditingController();
+
+  @observable
+  TextEditingController amountController = TextEditingController();
+
+  @observable
+  TextEditingController dateController = TextEditingController();
+
+  @observable
+  ObservableList<FinanceModel> financeList = ObservableList<FinanceModel>.of([]);
+
+  @observable
+  int type = 0;
+
+  @observable
+  double total = 0;
+
+  @observable
+  FinanceModel financeModel = FinanceModel(
+    id: const Uuid().v1(),
+    description: '',
+    amount: 0,
+    date: '',
+    type: 0,
+  );
+
+  @observable
+  DateTime focusedDay = DateTime.now();
+
+  @action
+  Future getFinanceList() async {
+    financeList.clear();
+    financeList.addAll(await financeController.queryRowsByDates(focusedDay));
+    calculateTotal();
+  }
+
+  @action
+  Future insertFinance() async {
+    FinanceModel financeModel = FinanceModel(
+      id: const Uuid().v1(),
+      description: descriptionController.text,
+      amount: double.parse(amountController.text),
+      date: dateController.text,
+      type: type, 
+    );
+    await financeController.insert(financeModel.toMap());
+    await getFinanceList();
+    clearControllers();
+  }
+
+  @action
+  Future insertFinanceFromSnackBar(FinanceModel financeModel) async {
+    await financeController.insert(financeModel.toMap());
+    await getFinanceList();
+    clearControllers();
+  }
+
+  @action
+  Future deleteFinance(String id) async {
+    await financeController.delete(id);
+    await getFinanceList();
+  }
+
+  @action
+  void initialDate() {
+    dateController.text = DateFormat("dd/MM/yyyy").format(DateTime.now());
+  }
+
+  @action
+  void changePage(int index) {
+    currentIndex = index;
+    pageController.animateToPage(index, duration: const Duration(milliseconds: 300), curve: Curves.ease);
+  }
+
+  @action
+  void changeDate(DateTime date) {
+    dateController.text = DateFormat("dd/MM/yyyy").format(date);
+  }
+
+  @action
+  void clearControllers() {
+    descriptionController.clear();
+    amountController.clear();
+  }
+
+  @action
+  void changeType(int index) {
+    type = index;
+  }
+
+  @action
+  void calculateTotal() {
+    total = 0;
+    for (FinanceModel financeModel in financeList) {
+      if (financeModel.type == 0) {
+        total += financeModel.amount;
+      } else {
+        total -= financeModel.amount;
+      }
+    }
+  }
+
+  @action
+  void changeFinanceModel(FinanceModel financeModel) {
+    this.financeModel = financeModel;
+  }
+
+  @action
+  void changeFocusedDay(DateTime focusedDay) {
+    this.focusedDay = focusedDay;
+    getFinanceList();
+  }
+
+  @action
+  void initStore() {
+    initialDate();
+    getFinanceList();
+  }
+
+}
---
+++ lib/view/store/finance_store.g.dart
@@ -0,0 +1,323 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'finance_store.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
+
+mixin _$FinanceStore on FinanceStoreBase, Store {
+  late final _$currentIndexAtom =
+      Atom(name: 'FinanceStoreBase.currentIndex', context: context);
+
+  @override
+  int get currentIndex {
+    _$currentIndexAtom.reportRead();
+    return super.currentIndex;
+  }
+
+  @override
+  set currentIndex(int value) {
+    _$currentIndexAtom.reportWrite(value, super.currentIndex, () {
+      super.currentIndex = value;
+    });
+  }
+
+  late final _$pageControllerAtom =
+      Atom(name: 'FinanceStoreBase.pageController', context: context);
+
+  @override
+  PageController get pageController {
+    _$pageControllerAtom.reportRead();
+    return super.pageController;
+  }
+
+  @override
+  set pageController(PageController value) {
+    _$pageControllerAtom.reportWrite(value, super.pageController, () {
+      super.pageController = value;
+    });
+  }
+
+  late final _$descriptionControllerAtom =
+      Atom(name: 'FinanceStoreBase.descriptionController', context: context);
+
+  @override
+  TextEditingController get descriptionController {
+    _$descriptionControllerAtom.reportRead();
+    return super.descriptionController;
+  }
+
+  @override
+  set descriptionController(TextEditingController value) {
+    _$descriptionControllerAtom.reportWrite(value, super.descriptionController,
+        () {
+      super.descriptionController = value;
+    });
+  }
+
+  late final _$amountControllerAtom =
+      Atom(name: 'FinanceStoreBase.amountController', context: context);
+
+  @override
+  TextEditingController get amountController {
+    _$amountControllerAtom.reportRead();
+    return super.amountController;
+  }
+
+  @override
+  set amountController(TextEditingController value) {
+    _$amountControllerAtom.reportWrite(value, super.amountController, () {
+      super.amountController = value;
+    });
+  }
+
+  late final _$dateControllerAtom =
+      Atom(name: 'FinanceStoreBase.dateController', context: context);
+
+  @override
+  TextEditingController get dateController {
+    _$dateControllerAtom.reportRead();
+    return super.dateController;
+  }
+
+  @override
+  set dateController(TextEditingController value) {
+    _$dateControllerAtom.reportWrite(value, super.dateController, () {
+      super.dateController = value;
+    });
+  }
+
+  late final _$financeListAtom =
+      Atom(name: 'FinanceStoreBase.financeList', context: context);
+
+  @override
+  ObservableList<FinanceModel> get financeList {
+    _$financeListAtom.reportRead();
+    return super.financeList;
+  }
+
+  @override
+  set financeList(ObservableList<FinanceModel> value) {
+    _$financeListAtom.reportWrite(value, super.financeList, () {
+      super.financeList = value;
+    });
+  }
+
+  late final _$typeAtom = Atom(name: 'FinanceStoreBase.type', context: context);
+
+  @override
+  int get type {
+    _$typeAtom.reportRead();
+    return super.type;
+  }
+
+  @override
+  set type(int value) {
+    _$typeAtom.reportWrite(value, super.type, () {
+      super.type = value;
+    });
+  }
+
+  late final _$totalAtom =
+      Atom(name: 'FinanceStoreBase.total', context: context);
+
+  @override
+  double get total {
+    _$totalAtom.reportRead();
+    return super.total;
+  }
+
+  @override
+  set total(double value) {
+    _$totalAtom.reportWrite(value, super.total, () {
+      super.total = value;
+    });
+  }
+
+  late final _$financeModelAtom =
+      Atom(name: 'FinanceStoreBase.financeModel', context: context);
+
+  @override
+  FinanceModel get financeModel {
+    _$financeModelAtom.reportRead();
+    return super.financeModel;
+  }
+
+  @override
+  set financeModel(FinanceModel value) {
+    _$financeModelAtom.reportWrite(value, super.financeModel, () {
+      super.financeModel = value;
+    });
+  }
+
+  late final _$focusedDayAtom =
+      Atom(name: 'FinanceStoreBase.focusedDay', context: context);
+
+  @override
+  DateTime get focusedDay {
+    _$focusedDayAtom.reportRead();
+    return super.focusedDay;
+  }
+
+  @override
+  set focusedDay(DateTime value) {
+    _$focusedDayAtom.reportWrite(value, super.focusedDay, () {
+      super.focusedDay = value;
+    });
+  }
+
+  late final _$getFinanceListAsyncAction =
+      AsyncAction('FinanceStoreBase.getFinanceList', context: context);
+
+  @override
+  Future<dynamic> getFinanceList() {
+    return _$getFinanceListAsyncAction.run(() => super.getFinanceList());
+  }
+
+  late final _$insertFinanceAsyncAction =
+      AsyncAction('FinanceStoreBase.insertFinance', context: context);
+
+  @override
+  Future<dynamic> insertFinance() {
+    return _$insertFinanceAsyncAction.run(() => super.insertFinance());
+  }
+
+  late final _$insertFinanceFromSnackBarAsyncAction = AsyncAction(
+      'FinanceStoreBase.insertFinanceFromSnackBar',
+      context: context);
+
+  @override
+  Future<dynamic> insertFinanceFromSnackBar(FinanceModel financeModel) {
+    return _$insertFinanceFromSnackBarAsyncAction
+        .run(() => super.insertFinanceFromSnackBar(financeModel));
+  }
+
+  late final _$deleteFinanceAsyncAction =
+      AsyncAction('FinanceStoreBase.deleteFinance', context: context);
+
+  @override
+  Future<dynamic> deleteFinance(String id) {
+    return _$deleteFinanceAsyncAction.run(() => super.deleteFinance(id));
+  }
+
+  late final _$FinanceStoreBaseActionController =
+      ActionController(name: 'FinanceStoreBase', context: context);
+
+  @override
+  void initialDate() {
+    final _$actionInfo = _$FinanceStoreBaseActionController.startAction(
+        name: 'FinanceStoreBase.initialDate');
+    try {
+      return super.initialDate();
+    } finally {
+      _$FinanceStoreBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void changePage(int index) {
+    final _$actionInfo = _$FinanceStoreBaseActionController.startAction(
+        name: 'FinanceStoreBase.changePage');
+    try {
+      return super.changePage(index);
+    } finally {
+      _$FinanceStoreBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void changeDate(DateTime date) {
+    final _$actionInfo = _$FinanceStoreBaseActionController.startAction(
+        name: 'FinanceStoreBase.changeDate');
+    try {
+      return super.changeDate(date);
+    } finally {
+      _$FinanceStoreBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void clearControllers() {
+    final _$actionInfo = _$FinanceStoreBaseActionController.startAction(
+        name: 'FinanceStoreBase.clearControllers');
+    try {
+      return super.clearControllers();
+    } finally {
+      _$FinanceStoreBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void changeType(int index) {
+    final _$actionInfo = _$FinanceStoreBaseActionController.startAction(
+        name: 'FinanceStoreBase.changeType');
+    try {
+      return super.changeType(index);
+    } finally {
+      _$FinanceStoreBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void calculateTotal() {
+    final _$actionInfo = _$FinanceStoreBaseActionController.startAction(
+        name: 'FinanceStoreBase.calculateTotal');
+    try {
+      return super.calculateTotal();
+    } finally {
+      _$FinanceStoreBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void changeFinanceModel(FinanceModel financeModel) {
+    final _$actionInfo = _$FinanceStoreBaseActionController.startAction(
+        name: 'FinanceStoreBase.changeFinanceModel');
+    try {
+      return super.changeFinanceModel(financeModel);
+    } finally {
+      _$FinanceStoreBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void changeFocusedDay(DateTime focusedDay) {
+    final _$actionInfo = _$FinanceStoreBaseActionController.startAction(
+        name: 'FinanceStoreBase.changeFocusedDay');
+    try {
+      return super.changeFocusedDay(focusedDay);
+    } finally {
+      _$FinanceStoreBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void initStore() {
+    final _$actionInfo = _$FinanceStoreBaseActionController.startAction(
+        name: 'FinanceStoreBase.initStore');
+    try {
+      return super.initStore();
+    } finally {
+      _$FinanceStoreBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+currentIndex: ${currentIndex},
+pageController: ${pageController},
+descriptionController: ${descriptionController},
+amountController: ${amountController},
+dateController: ${dateController},
+financeList: ${financeList},
+type: ${type},
+total: ${total},
+financeModel: ${financeModel},
+focusedDay: ${focusedDay}
+    ''';
+  }
+}
